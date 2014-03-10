555a0a067f7092e7f4c4f82a9c18b1c3ad78e85f
JWMAC <xodhks_0113@naver.com>
2014-03-10 17:20:01 +0900

    Demo3…

--- UICollectionViewDemo3/UICollectionViewDemo3/UICollectionViewDemo3ViewController.m
+++ UICollectionViewDemo3/UICollectionViewDemo3/UICollectionViewDemo3ViewController.m
@@ -4,7 +4,7 @@
 //
 //  Created by JWMAC on 2014. 3. 10..
 //  Copyright (c) 2014년 KimJiWook. All rights reserved.
-//
+// https://github.com/kimjiwook/WE_UICollectionViewDemos.git
 
 #import "UICollectionViewDemo3ViewController.h"
 
